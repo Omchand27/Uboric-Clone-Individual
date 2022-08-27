390d517bcfa1c4e05a2285716f09cf184a406763
omchand <omchandjoker27@gmail.com>
2022-08-27 23:58:49 +0530

    add Day-5

---
+++ productView.js
@@ -0,0 +1,71 @@
+
+let prodDetailsArr = [];
+prodDetailsArr.push(JSON.parse(localStorage.getItem("product-list")));
+
+function displayProd(prodDetailsArr){
+    // document.querySelector("#container").innerText= ""
+    prodDetailsArr.forEach(function(ele,i){
+        let image = document.createElement("img")
+        image.setAttribute("src", ele.Image)
+
+        let img3 = document.createElement("div")
+        
+        let img = document.createElement("img")
+        img.setAttribute("src", ele.Image)
+        img.addEventListener("click", function(){
+            let image = document.createElement("img")
+            image.setAttribute("src", ele.Image)
+        })
+
+        let img1 = document.createElement("img")
+        img1.setAttribute("src", ele.Image1)
+        img1.addEventListener("click", function(){
+            let image = document.createElement("img")
+            image.setAttribute("src", ele.Image1)
+        })
+
+        let img2 = document.createElement("img")
+        img2.setAttribute("src", ele.Image2)
+        img2.addEventListener("click", function(){
+            let image = document.createElement("img")
+            image.setAttribute("src", ele.Image2)
+        })
+
+        img3.append(img, img1, img2)
+        document.querySelector("#images").append(image,img3)
+
+        let brand = document.createElement("p")
+        brand.innerText=ele.Categories;
+
+        let title = document.createElement("h2")
+        title.innerText=ele.Name;
+
+        let price = document.createElement("h3")
+        price.innerText=ele.Price;
+        price.style.color="red"
+
+        let stock = document.createElement("p")
+        stock.innerText="(100 in stock)";
+
+        let quantity = document.createElement("input")
+        quantity.type="Number";
+        quantity.value="1";
+
+        let cart = document.createElement("button")
+        cart.innerText="Add to Cart";
+
+        let buy = document.createElement("button")
+        buy.innerText="Buy Now";
+
+        let wish = document.createElement("button")
+        wish.innerText="Add to Wishlist";
+
+        let Categories = document.createElement("p")
+        Categories.innerText="Categories:" + " " +ele.Categories;
+
+        document.querySelector("#details").append(brand, title, price, stock, quantity, cart, buy, wish, Categories)
+        
+    })
+}
+
+displayProd(prodDetailsArr);
